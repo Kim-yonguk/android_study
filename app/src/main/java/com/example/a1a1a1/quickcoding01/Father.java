76ef04f565ae84fcb27dda910e6ccacf9e4eb208
Kim-yonguk <1a1a11a@naver.com>
2016-10-31 21:48:05 +0900

    Revert "Quick01"
    This reverts commit 770bcd624814bb673db1d74b0c2a4b3ef56c958f.

--- app/src/main/java/com/example/a1a1a1/quickcoding01/Father.java
+++
@@ -1,9 +0,0 @@
-package com.example.a1a1a1.quickcoding01;
-
-/**
- * Created by 1a1a1 on 2016-10-06.
- */
-public abstract class Father {
-    public abstract int getResult();
-    int num[]={1,2,3,4,5,6,7,8,9,10};
-}
